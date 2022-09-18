92fee572178e3e74bdafe213a20ffc996698b846
Andres Felipe Rodriguez <andresfrodriguezo11@gmail.com>
2022-09-18 18:17:40 -0500

    Define an example of how to code a sigmoid function

--- logistic1.py
+++ logistic1.py
@@ -1,4 +1,25 @@
 import numpy as np
 
-N = 100
-D = 2
+#Adding some data to make the prediction later on
+N = 100 #100 Samples
+D = 2 #Dimension
+
+#In a matrix:
+X = np.random.randn(N,D)
+
+#Adding the bias term
+ones=np.array([[1]*N]).T
+Xb = np.concatenate((ones, X), axis=1)
+
+#Randomly initialize the weight vector
+
+w = np.random.randn(D+1)
+
+z = Xb.dot(w)
+
+#Let's apply the sigmoid
+
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+print(sigmoid(z))
